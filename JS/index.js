edf1a8ab0b74330a9ae99f7dc13be624cea629d4
Abrar Hosny <109631053+Abrar-Hosny@users.noreply.github.com>
2024-06-23 10:42:58 +0300

    add the flow of the program when there is missing data

--- JS/index.js
+++ JS/index.js
@@ -1,7 +1,51 @@
 var form = document.querySelector("form"); 
+var email = document.querySelector("#email");
+var password = document.querySelector("#password");
+var name = document.querySelector("#name");
+var btn = document.querySelector("button"); 
+var message =document.querySelector("#message")
+
+
+
 form.addEventListener("submit" , function(e){
     e.preventDefault(); 
 
 })
 
 
+
+btn.addEventListener("click" , function(){
+   if(email.value != null && name.value!=null && password!=null){
+   
+    if(email.classList.contains("is-valid") && password.classList.contains("is-valid")){
+        message.classList.replace("d-none" , "d-block");
+        message.innerHTML = "success";
+        message.classList.remove("")
+        
+        var emailValue = email.value ; 
+        var passwordValue = password.value ; 
+        
+        
+            }
+   }
+    else{
+message.classList.replace("d-none" , "d-block");
+message.innerHTML = "All inputs is required";
+
+    }
+})
+
+
+
+
+
+
+
+function validation(){
+regex={
+    email:/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/, 
+    password :/[a-zA-Z0-9@]{6}/
+}
+
+
+}
