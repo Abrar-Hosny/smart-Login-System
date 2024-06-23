5bbfed36bcc1da48e74a807b45f0e2105c2082a2
Abrar Hosny <109631053+Abrar-Hosny@users.noreply.github.com>
2024-06-23 13:09:32 +0300

    check if htere is accounts in the local storage or not if yes return them

--- JS/login.js
+++ JS/login.js
@@ -1,8 +1,13 @@
 emailInput = document.getElementById("email");
 passwordInput = document.getElementById("password");
 btn = document.querySelector("#btn");
-
+form = document.querySelector("form");
 var accountsExsist ;
+
+form.addEventListener("submit" , function(event){
+    event.preventDefault() ; 
+
+})
 
 btn.addEventListener("click" ,function(){
 if(localStorage.getItem("accounts") == null){
@@ -10,6 +15,9 @@ if(localStorage.getItem("accounts") == null){
 }
 else{
     accountsExsist=JSON.parse(localStorage.getItem("accounts"));
-    console.log(accountsExsist)
+    console.log(accountsExsist);
 }
+
 })
+
+
