395a65d720ec88ffc64d4a4a43bfc48e2c32ed7c
Abrar Hosny <109631053+Abrar-Hosny@users.noreply.github.com>
2024-06-23 11:37:58 +0300

    add the flow of the program when there is missing data

--- JS/index.js
+++ JS/index.js
@@ -1,9 +1,10 @@
 var form = document.querySelector("form"); 
+var nameInput = document.querySelector("#name");
 var email = document.querySelector("#email");
 var password = document.querySelector("#password");
-var name = document.querySelector("#name");
 var btn = document.querySelector("button"); 
-var message =document.querySelector("#message")
+var message =document.querySelector("#message"); 
+var allInputs = document.querySelectorAll("input"); 
 
 
 
@@ -15,11 +16,13 @@ form.addEventListener("submit" , function(e){
 
 
 btn.addEventListener("click" , function(){
-   if(email.value != null && name.value!=null && password!=null){
+   if(email.value != "" && nameInput.value!="" && password!=""){
+    console.log(email.value , nameInput.value , password.value);
+
     message.classList.replace("d-none" , "d-block");
     message.innerHTML = "success";
     message.classList.remove("text-danger");
-    message.classList.add("text-info");
+    message.classList.add("text-success");
     
     var emailValue = email.value ; 
     var passwordValue = password.value ; 
@@ -30,6 +33,7 @@ btn.addEventListener("click" , function(){
     //         }
    }
     else{
+
 message.classList.replace("d-none" , "d-block");
 message.innerHTML = "All inputs is required";
 
@@ -38,15 +42,32 @@ message.innerHTML = "All inputs is required";
 
 
 
+for (var i =0 ; i < allInputs.length ; i++){
+//    IIFE => immediately invoked function expression 
+// will help to catch the current i beacause the var key word we can solve the problem 
+// with 2 ways 1- change var to let or add iife
 
 
+    (function(i){
+        allInputs[i].addEventListener("input" , function(){ 
+            var regex = {
+                name:/[^@\.\-0-9][a-zA-Z]{3,8}$/,
+                email:/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/, 
+                password :/[a-zA-Z0-9@]{6}/
+            }
+            
+    if(regex[allInputs[i].id].test(allInputs[i].value)){
+        console.log("yess")
 
+    }
+    else{
+        console.log("Ni")
 
-function validation(){
-regex={
-    email:/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/, 
-    password :/[a-zA-Z0-9@]{6}/
+    }
+    
+    })
+   })(i);
 }
 
 
-}
+
